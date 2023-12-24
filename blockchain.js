5e11c868da2056198fbe98f185e9feb8171a0605
Francisco Jair <fjair123@gmail.com>
2023-12-24 10:12:09 -0300

    add chain

--- blockchain.js
+++ blockchain.js
@@ -35,6 +35,19 @@ class Blockchain {
 
     return true;
   }
+
+  replaceChain(newChain) {
+    // Verifica se a nova cadeia é maior que a cadeia atual
+    if (newChain.length <= this.chain.length) {
+      console.log("Receive chain is not longer than the currnet chain.");
+      return;
+    } else if (!this.isValidChain(newChain)) {
+      console.log("The received chain is not valid.");
+      return;
+    }
+    console.log("Replacing blockchain with the new chain.");
+    this.blockchain = newChain;
+  }
 }
 
 module.exports = Blockchain;
